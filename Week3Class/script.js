0b862974875ffd559ca9e992673a2704677d2e2d
Firmiana-VC75 <ywu22@inside.artcenter.edu>
2024-09-27 13:29:30 -0700

    week3process

--- Week3Class/script.js
+++ Week3Class/script.js
@@ -1,7 +1,33 @@
 let DOMoptions=document.getElementById("DOMoptions")
 let newParagraph = document.createElement("p")
-
-DOMoptions.style.backgroundColor = "yellow"
+let buttonColor=document.getElementById('btnColorChange')
+let toggleImages = document.getElementById("btnImageToggle")
+let galleryIMG = document.getElementById("imageGallery").children[0]
 
 newParagraph.innerText="Hello,World!"
 DOMoptions.appendChild(newParagraph)
+
+let imageToggle = function(){
+    console.log("fire!")
+    galleryIMG.src = "images/rabbit2.jpeg"
+    console.log(galleryIMG.src)
+
+    if(galleryIMG.src.includes("rabbit1")){
+        console.log("rabbit1")
+    }
+    else{
+        console.log("rabbit2")
+    }
+
+}
+toggleImages.addEventListener("click", imageToggle)
+
+buttonColor.addEventListener("click", function(){
+    let redPortion = Math.random() * 255
+    let greenPortion = Math.random() * 255
+    let bluePortion = Math.random() * 255
+    let randomColor = "rgb(" + redPortion + ", "+ greenPortion +", "+ bluePortion +")"
+    console.log(randomColor)
+
+    DOMoptions.style.backgroundColor = randomColor
+})
